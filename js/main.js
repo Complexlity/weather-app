a4679454771c16b3b48fa409ffa9f13c4df31f62
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-21 22:48:29 +0100

    Add debug code for ios mobile

--- js/main.js
+++ js/main.js
@@ -64,13 +64,16 @@ async function loadSite(cityName) {
 async function getDefaultLocation() {
   if (navigator.geolocation) {
     // device can return its location
+    alert("I am here 1");
     function success(position) {
+      alert("I am here 2");
       lat = position.coords.latitude;
       lon = position.coords.longitude;
       loadSite([lat, lon]);
     }
 
     function error(err) {
+      alert("I am here 3");
       alert("Location data unauthorized!! Rerouting to random location");
       let randomCity = getRandomCity();
       loadSite(randomCity);
